c3594122895c6ac60936ad9d137a179c49be64ca
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2023-04-30 16:53:47 -0500

    Javascript for 3rd API

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function fetching(charId) {
   const ts = 1;
   const hash = hashing(ts, publicKey, privateKey);
   const url = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&name=${charId}`;
-  
+ 
+  const resultsContainer = document.getElementById("resultsContainer")
   fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -29,6 +30,8 @@ function fetching(charId) {
         card.appendChild(description);
         
         resultsContainer.appendChild(card);
+
+        console.log(data)
       });
     })
     .catch(error => console.log(error));
@@ -46,7 +49,7 @@ const resultsContainer = document.getElementById("result-container");
 searchButton.addEventListener("click", function() {
   const query = searchInput.value;
   const url = `https://gateway.marvel.com/v1/public/characters?name=${query}&apikey=${publicKey}`;
-
+//Wikipedia fetch
   fetching(query);
 
   var wiki= document.getElementById("search-box").value
@@ -57,8 +60,83 @@ searchButton.addEventListener("click", function() {
    .then((reponse) =>{
        return reponse.json();
    })
-   .then((data) => {
-      console.log(data)
-   })})
-  
+   .then(async (data) => {
 
+console.log(data);
+
+var pageID = Object.keys(data.query.pages)[0]
+data.query.pages[pageID];
+
+console.log(pageID)
+
+    
+const infoWiki = data.query.pages[pageID].extract;
+//Name from Search Bar
+    var heroName= document.getElementById("heroNameTitle")
+    heroName.innerHTML= searchInput.value.toUpperCase();
+//Search bar input from Wikipedia
+    var wikiInfo = document.getElementById("wikiInfo")
+    wikiInfo.innerHTML = infoWiki
+
+//fetch powerstats
+function fetchSuperheroApiData(resultsContainer, card) {
+  var apiKey = "10109737487404041";
+  var heroApiUrl = `https://www.superheroapi.com/api.php/${apiKey}/search/${resultsContainer}`
+
+  fetch(heroApiUrl)
+   .then((reponse) =>{
+       return reponse.json();
+   })
+   .then(async (data) => {
+
+console.log(data);
+    
+fetch(heroApiUrl)
+.then(response => response.json())
+.then(data => {
+  var heroData = data.results && data.results[0];
+  if (!heroData) {
+    console.log('No data found on Superhero API.');
+    return;
+  }
+
+  console.log(heroData)
+
+  //var powerstats = heroData.powerstats;
+  //var appearance = heroData.appearance;
+  //var connections = heroData.connections;
+
+  //var powerstatsSection = createDataSection('Powerstats', powerstats);
+  //var appearanceSection = createDataSection('Appearance', appearance);
+  //var connectionsSection = createDataSection('Connections', connections);
+
+  //card.appendChild(powerstatsSection);
+  //card.appendChild(appearanceSection);
+  //card.appendChild(connectionsSection);
+//})
+//.catch(error => console.log(error));
+//}
+
+//function createDataSection(title, data) {
+//var section = document.createElement("section");
+//section.classList.add("data-section");
+
+//var heading = document.createElement("h3");
+//heading.innerText = title;
+//section.appendChild(heading);
+
+//var list = document.createElement("ul");
+//for (var key in data) {
+//var listItem = document.createElement("li");
+//listItem.innerText = `${key}: ${data[key]}`;
+//list.appendChild(listItem);
+//}
+//section.appendChild(list);
+//return section;
+//}
+})})}})})
+      
+   
+  
+  
+  
